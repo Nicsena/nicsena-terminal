754acca3c3f8eba185a657d96a5b842334222de8
Nicsena <Nicholassena8190@gmail.com>
2023-01-05 01:05:31 -0800

    1.0.1 Release

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ const user = os.userInfo().username;
 const terminal_name = os.hostname();
 
 
-
-
-
 // ---- EXPRESS SERVER ----
 app.set("view engine", "ejs"); 
 
@@ -28,9 +25,31 @@ app.enable("trust proxy");
 
 app.use(bodyParser.urlencoded({ extended: false })); 
 
+let requests = 1;
+
+app.all('*', function (req, res, next) { 
+
+   var IP = req.ip || req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'].split(",")[0];
+   var timestring = new Date().toLocaleString()
+   var UserAgent = req.get('User-Agent')
+   var Path = req.path
+   var Method = req.method
+   var Version = req.httpVersion
+   var Host = req.hostname
+   
+  console.log(`Method: ${Method} - HTTP ${Version} - Time: ${timestring} - Requests: ${requests++} - IP: ${IP} - Path: ${Path} - User Agent: ${UserAgent}`);
+  
+  next();
+
+}); 
+
 
 app.get("/", (req, res) => { 
     res.render("index", {user:'' +user +'',term_name:'' +terminal_name +''}) 
+}); 
+
+app.get("/updates", (req, res) => { 
+    res.render("updates") 
 }); 
 
 
@@ -79,21 +98,26 @@ wss.on('connection', function connection(ws, req) {
     const result = isBinary ? message : message.toString() 
     const data = JSON.parse(result)
     
-   // console.log(`Client ${ip} - Received: ${result}`);
+    //console.log(`Client ${ip} - Received: ${result}`);
     
     if(data["event"] === "connected") { 
-      return ws.send({ "event": "hello" } ) 
+      return ws.send(JSON.stringify( [ { "event": "hello" } ] )  )
     }
     
     if(data["event"] === "ping") {
-      return ws.send({ "event": "pong" } ) 
+    return ws.send(JSON.stringify( [ { "event": "pong" } ] ) ) 
     };
     
     if(data["event"] === "command") {
-      //console.log(data["args"]); 
-      //return ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": data["args"] } ] ) )
-      return runprogram(data["args"], ws)
+      console.log(`${ip} - Running command: ${data["args"]}`);
+      //return ws.send(JSON.stringify( [ { "event": "consoleoutput", "args": data["args"] } ] ) )
+      return runProgram(data["args"], ws)
     };
+
+    if(data["event"] === "stopcommand") {
+      console.log(`${ip} - Killing a process with PID: ${data["args"]}`)
+      return process.kill(data["args"], 'SIGTERM') 
+    }
     
     
 });
@@ -115,25 +139,90 @@ console.log(`Websocket - A client has connected! - client IP Address: ${ip}`)
 
 // --------- FUNCTIONS: --------
 
-async function runprogram(program, ws) {
-  
-exec(`${program}`, (error, stdout, stderr) => {
 
-if(stderr) {
- ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": stderr } ] ) )
-}
+function runProgram(program, ws) {
   
-if(stdout) {
- console.log(stdout)
- ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": stdout } ] ) )
-}
-  
-if(error) {
- console.log(error)
- //ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": `Error: ${error.code} - cmd: ${error.cmd}` } ] ) )
-}
+const pro = exec(program)
+
+ws.send(JSON.stringify([
+  {
+    "event": "programstart",
+    "args": `${pro.pid}`
+  }
+]));
+
+
+pro.stdout.on('data', (data => {
+  console.log(`${program} (PID: ${pro.pid}) STDOUT: ${data}`)
+
+  ws.send(JSON.stringify([
+    {
+      "event": "consoleoutput",
+      "program": program,
+      "pid": pro.pid,
+      "args": `${data}`
+    }
+  ]));
+
+}));
+
+pro.stderr.on('data', (data => {
+  console.error(`${program} (PID: ${pro.pid}) STDERR: ${data}`);
+
+  ws.send(JSON.stringify([
+    {
+      "event": "consoleoutput",
+      "args": `${data}`
+    }
+  ]));
+
+}));
+
+pro.on('close', (code, singal) => {
+  console.log(`${program} - ${pro.pid} - ${code} - ${singal}`)
+
+  if(singal === "SIGTERM") {
+    ws.send(JSON.stringify([
+      {
+        "event": "consoleoutput",
+        "args": `\nProcess has been killed.\n`
+      }
+    ]));
+  }
+
+  ws.send(JSON.stringify([
+    {
+      "event": "programstop",
+      "args": `${pro.pid}`
+    }
+  ]));
+
+});
+
+pro.on('error', (err) => {
+  console.log(err)
   
 });
+
+
+  
+// exec(`${program}`, (error, stdout, stderr) => {
+
+// if(stderr) {
+//  ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": stderr } ] ) )
+// }
+  
+// if(stdout) {
+//  console.log(stdout)
+//  ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": stdout } ] ) )
+// }
+  
+// if(error) {
+//  console.log(error)
+//  //ws.send(JSON.stringify ( [ { "event": "consoleoutput", "args": `Error: ${error.code} - cmd: ${error.cmd}` } ] ) )
+// }
+  
+// });
   
 }
 
